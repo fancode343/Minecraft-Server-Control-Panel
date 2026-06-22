a5e5c186c396e4f79a82b491bd0472ca2c37289f
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-06-22 14:53:59 +0000

    Code updated

--- MinecraftCPJava/ControlPanel/src/Claculator.java
+++ MinecraftCPJava/ControlPanel/src/Claculator.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first number: ");
-        String num1 = input.nextInt();
+        String num1 = input.nextLine();
         System.out.print("Enter second number: ");
-        String num2 = input.next
+        String num2 = input.nextLine();
+        System.out.println("Answer: "+num1+num2);
     }
 }
