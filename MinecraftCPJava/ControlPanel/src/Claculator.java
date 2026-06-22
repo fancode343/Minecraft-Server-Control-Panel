c70c21698194fe84742715bd6e01cb5240d653d6
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-06-22 14:37:06 +0000

    Commit

---
+++ MinecraftCPJava/ControlPanel/src/Claculator.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the 1st number: ");
+        String num1 = input.nextInt();
+    }
+}
