80576449667080b2a5959fd938a3ed11261b011e
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-08-13 15:16:56 +0000

    Added some logs

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 
-//LoadPlayers
-const loadPlayers = require("./services/loadPlayers");
-const MCbot = require("./services/MCbot");
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -17,13 +13,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+console.log("Loading services: ");
+const servicesPath = path.join(__dirname, "services");
+fs.readdirSync(servicesPath).forEach((file) => {
+  if (file.endsWith(".js")) {
+    console.log(`   - ${file}`);
+  }
+});
+console.log("All services loaded successfully.");
+
 // Routes — auto-load every .js file in ./routes
 const routesPath = path.join(__dirname, "routes");
+console.log("Loading routes: ");
 fs.readdirSync(routesPath).forEach((file) => {
   if (file.endsWith(".js")) {
     app.use(require(path.join(routesPath, file)));
+    console.log(`   - ${file}`);
   }
 });
+console.log("All routes loaded successfully.");
+
 
 app.get("/", require("./middleware/auth"), (req, res) => res.redirect("/dashboard"));
 app.get("/logout", (req, res) => req.session.destroy(() => res.redirect("/auth")));
